023c729a8b6b1371d612204acdf52d83db96d95a
Zikz13 <113542931+Zikz13@users.noreply.github.com>
2022-09-15 03:09:36 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -28,13 +28,13 @@ export const config = {
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: "omve75zhZcyXsNKDsbZbBb76NknE",
       // 你想对他发送的模板消息的模板ID
-      useTemplateId: "OWy-d1WRZlirR8EZrzJsXufJKiVo6E1ZNkg3Gmg4xa0",
+      useTemplateId: "sqvJrTe8OKj_u4VcGQMZvPn4v8N1mkULkzVRxyRu1pY",
       // 所在省份
       province: "湖北",
       // 所在城市
       city: "孝感",
       // 新历生日, 仅用作获取星座运势, 格式必须
-      horoscopeDate: '12-16',
+      horoscopeDate: '2-28',
       // 获取什么时候的星座运势，可选：['今日', '明日', '本周', '本月', '今年'], 留空则随机
       horoscopeDateType: '今日',
       // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
