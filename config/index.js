20bc65f5ac5aaff1344909ae1ecb074e407f834b
Zikz13 <113542931+Zikz13@users.noreply.github.com>
2022-09-15 02:30:30 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -23,9 +23,10 @@ export const config = {
   USERS: [
     {
       // 想要发送的人的名字
-      name: "omve751-KsKjHQOZInMMnFX3-XH4",
+      name: "乖乖",
+        
       // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-    
+      id: "omve75zhZcyXsNKDsbZbBb76NknE",
       // 你想对他发送的模板消息的模板ID
       useTemplateId: "itDWP8y-caFyviWGEVnkq_s7Ii9XPOkKGoldxbYfIEg",
       // 所在省份
